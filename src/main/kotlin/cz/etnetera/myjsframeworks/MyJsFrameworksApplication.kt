cb7c277c4ffaed6720c3fb70b5af67f70e720cb3
Vladimir Nevyhosteny <vnevyhosteny@gmail.com>
2024-05-20 13:18:42 +0200

    inital commit

---
+++ src/main/kotlin/cz/etnetera/myjsframeworks/MyJsFrameworksApplication.kt
@@ -0,0 +1,11 @@
+package cz.etnetera.myjsframeworks
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MyJsFrameworksApplication
+
+fun main(args: Array<String>) {
+	runApplication<MyJsFrameworksApplication>(*args)
+}
---
+++ src/test/kotlin/cz/etnetera/myjsframeworks/MyJsFrameworksApplicationTests.kt
@@ -0,0 +1,13 @@
+package cz.etnetera.myjsframeworks
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MyJsFrameworksApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
